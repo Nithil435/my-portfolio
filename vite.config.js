d2946be728638dd8ab7c556f59952ecb8efcfe18
Nithil435 <nithilvarma435@gmail.com>
2025-01-25 22:05:18 +0530

    Update files: App, images, config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { SitemapPlugin } from 'vite-plugin-sitemap';
 
+// Determine if we are deploying to GitHub Pages
 const isGitHubPages = process.env.NODE_ENV === 'production' && process.env.BASE === 'github';
 
 export default defineConfig({
-  plugins: [react()],
-  base: isGitHubPages ? '/my-portfolio/' : '/', // Adjust base path
+  plugins: [
+    react(),
+    SitemapPlugin({
+      hostname: 'https://my-portfolio-chi-livid-74.vercel.app', // Replace with your actual hostname
+      routes: [
+        '/',        // Home
+        '/about',   // Example route for the About page
+        '/projects' // Example route for a Projects page
+      ],
+    }),
+  ],
+  base: isGitHubPages ? '/my-portfolio/' : '/', // Adjust base path for deployment
 });
